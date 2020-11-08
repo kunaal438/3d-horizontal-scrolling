0501c67e6a90a8a8c7a8ddae56eb3421ad5d03f2
kunaal438 <kunaal438@gmail.com>
2020-11-08 18:54:44 +0530

    files added

---
+++ In Video/app.js
@@ -0,0 +1,56 @@
+const content = [...document.querySelectorAll('.container .content')];
+
+const nxtBtn = document.querySelector('.nxt-btn');
+const preBtn = document.querySelector('.pre-btn');
+
+let i = 0;
+
+window.onload = () => {
+    window.scrollTo(0, 0);
+}
+
+const slideNxt = () => {
+    if(i < 3 && !nxtBtn.className.includes('disable')){
+        content[i].classList.add('rotate');
+        content[i+1].classList.add('rotate');
+
+        setTimeout(() => {
+            content.map(item => item.className = 'content');
+            nxtBtn.classList.remove('disable');
+            preBtn.classList.remove('disable');
+        }, 2000);
+
+        i++;
+        setTimeout(() => {
+            window.scrollTo(window.innerWidth*i, 0);
+        }, 1000);
+    }
+}
+
+const slidepre = () => {
+    if(i > 0 && !preBtn.className.includes('disable')){
+        content[i].classList.add('rotate-back');
+        content[i-1].classList.add('rotate-back');
+
+        setTimeout(() => {
+            content.map(item => item.className = 'content');
+            nxtBtn.classList.remove('disable');
+            preBtn.classList.remove('disable');
+        }, 2000);
+
+        i--;
+        setTimeout(() => {
+            window.scrollTo(window.innerWidth*i, 0);
+        }, 1000);
+    }
+}
+
+nxtBtn.addEventListener('click', () => {
+    slideNxt()
+    nxtBtn.classList.add('disable');
+})
+
+preBtn.addEventListener('click', () => {
+    slidepre();
+    preBtn.classList.add('disable');
+})
